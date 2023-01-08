730b06b7d46ae43d62ffd5d99bcdba7785c24f7e
bdolan55 <brian.e.dolan88@gmail.com>
2023-01-08 19:07:37 +0000

    score incrementation added for player winning and for when the computer wins and is working

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,28 +1,49 @@
     const availableSelections = ["rock", "paper", "scissors"];  
     const playerScore = 0;
     const compScore = 0;
-    const playerScoreTag = document.getElementById("playerScore");
-    const compScoreTag = document.getElementById("compScore");
+  /*  const playerScoreTag = document.getElementById("playerScore");
+    const compScoreTag = document.getElementById("compScore"); */
     const scoringArea = document.querySelector(".scores-area");
     const resultOutput = document.querySelector(".result_txt");
     const rockHand = document.getElementById("rock");
     const paperHand = document.getElementById("paper");
     const scissorsHand = document.getElementById("scissors");   
 
+
+
     function gameChoice(playerChoice){
         const chosenCompSelection = compSelection();
         if (playerChoice === chosenCompSelection) {
-            console.log("its a Tie");
+            playerDraw();
           } else if (
             (playerChoice === 'rock' && chosenCompSelection === 'scissors') ||
             (playerChoice === 'paper' && chosenCompSelection === 'rock') ||
             (playerChoice === 'scissors' && chosenCompSelection === 'paper')
           ) {
-            console.log("Player Wins!!");
+            playerWins();
           } else {
-            console.log("Computer Wins!!");
+            compWins();
           }
     }
+
+
+
+    function playerDraw() {
+        console.log("Its a Draw");
+    }
+
+
+    function playerWins() {
+        let oldScore = parseInt(document.getElementById("playerScore").innerText);
+        document.getElementById("playerScore").innerText = ++oldScore;
+    }
+
+    
+    function compWins() {
+        let oldScore = parseInt(document.getElementById("compScore").innerText);
+        document.getElementById("compScore").innerText = ++oldScore;
+    }
+
 
     function compSelection () {
         const randomCompChoice = Math.floor(Math.random() * availableSelections.length);
