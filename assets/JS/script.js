a7bcf28403bac4d171f9c512d869b10affef5cf8
bdolan55 <brian.e.dolan88@gmail.com>
2023-01-08 09:58:53 +0000

    function code put into ensure dom is cleared before game runs and also event listeners with alerts to show correct button/data-type has when rock paper scissors or submit is clicked

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,25 @@
+/**
+ * function to wait for the DOM to stop loading 
+ * Added event listeners to show which button was clicked ie. Rock,Paper,Scissors or Fight(submit)
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "fight") {
+                alert("You clicked fight");
+            } else {
+                let handChoice = this.getAttribute("data-type");
+                alert(`You clicked ${handChoice}`); 
+            }
+        })
+    }
+})
+
+
+
 function runGame() {
 
 
