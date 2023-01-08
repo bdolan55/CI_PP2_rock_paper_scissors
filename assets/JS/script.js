ed8aaca418b740117f35e4c52da5c62ae8828cb3
bdolan55 <brian.e.dolan88@gmail.com>
2023-01-08 20:22:37 +0000

    HTML inner result text xhanges to show who won the round

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,12 @@
     const availableSelections = ["rock", "paper", "scissors"];  
     const playerScore = 0;
     const compScore = 0;
-  /*  const playerScoreTag = document.getElementById("playerScore");
-    const compScoreTag = document.getElementById("compScore"); */
     const scoringArea = document.querySelector(".scores-area");
     const resultOutput = document.querySelector(".result_txt");
     const rockHand = document.getElementById("rock");
     const paperHand = document.getElementById("paper");
     const scissorsHand = document.getElementById("scissors");   
+    const letsFight = document.getElementById("fight");
 
 
 
@@ -24,24 +23,26 @@
           } else {
             compWins();
           }
-    }
-
-
+    } 
 
     function playerDraw() {
         console.log("Its a Draw");
+        resultOutput.innerHTML = "ITS A DRAW";
     }
 
 
     function playerWins() {
         let oldScore = parseInt(document.getElementById("playerScore").innerText);
         document.getElementById("playerScore").innerText = ++oldScore;
+        resultOutput.innerHTML = "you win winner";
+
     }
 
     
     function compWins() {
         let oldScore = parseInt(document.getElementById("compScore").innerText);
         document.getElementById("compScore").innerText = ++oldScore;
+        resultOutput.innerHTML = "you lose loser";
     }
 
 
@@ -51,7 +52,6 @@
     }
 
     function runGame(){
-
             rockHand.addEventListener('click', function() {
                 gameChoice("rock")
             })
@@ -63,4 +63,4 @@
             })
         }
 
-        runGame("rock");
+        runGame();
